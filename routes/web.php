11ea8ff6e1faba37d8efc4912aca1c60e13f1912
SSatuki <130502satsuki@gmail.com>
2023-02-12 21:42:38 +0900

    08-2

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [PostController::class,'index']);{
+    return view('posts/index');
+};
