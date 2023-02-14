a2d3be3942054abb98f8be1f18a6df1597247658
SSatuki <130502satsuki@gmail.com>
2023-02-14 21:13:20 +0900

    08-3

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\PostController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', [PostController::class,'index']);{
-    return view('posts/index');
-};
+Route::get('/', [PostController::class,'index']);
+Route::get('/posts/{post}',[PostController::class, 'show']);
