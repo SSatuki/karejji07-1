e5e82b97329e8bc1066752c53a7283368c865097
SSatuki <130502satsuki@gmail.com>
2023-02-16 14:01:19 +0900

    08-4

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -12,9 +12,9 @@ class Post extends Model
     protected $fillable = [
         'title',
         'body'
-        ];
+    ];
     
-    public function getPaginateByLimit(int $limit_count = 5)
+    public function getPaginateByLimit(int $limit_count = 10)
     {
         return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
     }
